4be502e8803be0ad0ce393f27293c75c392d3ee4
kalpeshdeora1006 <kalpeshdeora1006@gmail.com>
2025-10-23 11:10:37 +0530

    Create ManagerDashboard.jsx

---
+++ frontend/src/components/ManagerDashboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const ManagerDashboard = () => (
+  <div>
+    <h2>Manager Dashboard</h2>
+    {/* Show live orders, history, billing here next! */}
+  </div>
+);
+export default ManagerDashboard;
