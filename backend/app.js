99c4c8f97c1575fda662f00d6de0b7e0f8a7f73c
kalpeshdeora1006 <kalpeshdeora1006@gmail.com>
2025-10-23 10:53:59 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const orderRoutes = require('./routes/order');
+const authRoutes = require('./routes/auth');
+
+app.use('/api/orders', orderRoutes);
+app.use('/api/auth', authRoutes);
+
+module.exports = app;
 
