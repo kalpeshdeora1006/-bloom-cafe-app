3f8830a774b058f5ae25f9751a0f8b1224b7aae1
kalpeshdeora1006 <kalpeshdeora1006@gmail.com>
2025-10-23 11:10:09 +0530

    Update KitchenDashboard.jsx

--- frontend/src/components/KitchenDashboard.jsx
+++ frontend/src/components/KitchenDashboard.jsx
@@ -1,1 +1,9 @@
+import React from 'react';
+const KitchenDashboard = () => (
+  <div>
+    <h2>Kitchen Dashboard</h2>
+    {/* Add order entry and menu here next! */}
+  </div>
+);
+export default KitchenDashboard;
 
