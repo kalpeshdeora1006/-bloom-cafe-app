4040a69cb9e895f3de9c279b9cadd43e0416ffd4
kalpeshdeora1006 <kalpeshdeora1006@gmail.com>
2025-10-23 10:56:02 +0530

    Update order.js

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const Order = require('../models/Order');
+
+router.post('/', async (req, res) => {
+  const order = new Order(req.body);
+  await order.save();
+  res.json(order);
+});
+
+router.get('/', async (req, res) => {
+  const orders = await Order.find();
+  res.json(orders);
+});
+
+router.put('/:id', async (req, res) => {
+  const order = await Order.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(order);
+});
+
+module.exports = router;
 
